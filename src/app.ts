bed370c2506c0bd14fce9761fe6c6827538990f6
Cyber_Infernox <sayonkar@gmail.com>
2023-10-26 18:26:29 +0530

    Added typescript logic

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { Invoice } from "./Classes/Invoice.js";
+import { ListTemplate } from "./Classes/ListTemplate.js";
 import { Payment } from "./Classes/Payment.js";
 import { HasFormatter } from "./Interfaces/HasFormatter.js";
 
@@ -8,6 +9,9 @@ const type = document.querySelector("#type") as HTMLSelectElement;
 const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
+
+const ul = document.querySelector("ul")!;
+const list = new ListTemplate(ul);
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -19,5 +23,5 @@ form.addEventListener("submit", (e) => {
     doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
   }
 
-  console.log(doc);
+  list.render(doc, type.value, "end");
 });
